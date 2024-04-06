f83a8575d4bcc6f15a2c3f14385f4f521654aa26
Aarzoo <aarzoogarg172@gmail.com>
2024-04-06 11:12:47 +0530

    Project setup

---
+++ EmployEase/src/main/java/com/hirehub/EmployEase/EmployEaseApplication.java
@@ -0,0 +1,13 @@
+package com.hirehub.EmployEase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployEaseApplication.class, args);
+	}
+
+}
---
+++ EmployEase/src/test/java/com/hirehub/EmployEase/EmployEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hirehub.EmployEase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
