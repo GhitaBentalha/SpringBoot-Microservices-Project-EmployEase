492bbe85c6f23ac240182593b81c7bee9a20fb44
Aarzoo1703 <aarzoogarg172@gmail.com>
2024-04-12 22:49:48 +0530

    job application entity

--- EmployEase/src/main/java/com/hirehub/EmployEase/jobapplication/JobApplication.java
+++ EmployEase/src/main/java/com/hirehub/EmployEase/jobapplication/JobApplication.java
@@ -4,19 +4,15 @@ import java.time.LocalDateTime;
 
 import com.hirehub.EmployEase.alluser.User;
 import com.hirehub.EmployEase.job.Job;
-
-import jakarta.persistence.Embeddable;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 
-@Data
-@Embeddable
 @Entity
-@AllArgsConstructor
 public class JobApplication {
+
     @Id
     public String jobApplicationId;
 
@@ -25,12 +21,61 @@ public class JobApplication {
 
     @ManyToOne
     private Job job;
+   
+    @Enumerated(EnumType.STRING)
     private JOB_APPLICATION_STATUS status;
+
+    public JobApplication(String jobApplicationId, User user, Job job, JOB_APPLICATION_STATUS status,
+            LocalDateTime appliedDate) {
+        this.jobApplicationId = jobApplicationId;
+        this.user = user;
+        this.job = job;
+        this.status = status;
+        this.appliedDate = appliedDate;
+    }
 
     private LocalDateTime appliedDate;
 
-    public JobApplication() {
-        this.jobApplicationId = ApplicationIdGenerator.generateId(10);
+    public String getJobApplicationId() {
+        return jobApplicationId;
     }
 
+    public void setJobApplicationId(String jobApplicationId) {
+        this.jobApplicationId = jobApplicationId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Job getJob() {
+        return job;
+    }
+
+    public void setJob(Job job) {
+        this.job = job;
+    }
+
+    public JOB_APPLICATION_STATUS getStatus() {
+        return status;
+    }
+
+    public JobApplication() {
+    }
+
+    public void setStatus(JOB_APPLICATION_STATUS status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getAppliedDate() {
+        return appliedDate;
+    }
+
+    public void setAppliedDate(LocalDateTime appliedDate) {
+        this.appliedDate = appliedDate;
+    }
 }
